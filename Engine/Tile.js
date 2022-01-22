d05241431870d28d108028905aa5d364693ca91d
flobbun <flobbun@hotmail.com>
2022-01-22 22:29:18 +0000

    backup

--- Engine/Tile.js
+++ Engine/Tile.js
@@ -1,16 +1,34 @@
 export default class Tile{
     
-    constructor(x, y, src, size){
+    constructor(x, y, src, size, tileRow, tileCol, worldX, worldY){
+        this.worldX = worldX;
+        this.worldY = worldY;
         this.x = x;
         this.y = y;
         this.src = src;
         this.size = size;
+        this.tileRow = tileRow;
+        this.tileCol = tileCol;
         this.image = new Image();
         this.image.src = this.src;
+        this.tilesetWidth = this.image.width;
+        this.tilesetHeight = this.image.height;
     }
 
     draw(ctx, progress){
-        ctx.drawImage(this.image, this.x, this.y, this.size, this.size);
+        ctx.drawImage
+        (
+            this.image,                  // Tileset image
+            (this.tileCol * this.size),  // The x coordinate where to start clipping
+            (this.tileRow * this.size),  // The y coordinate where to start clipping
+            this.size,                   // How stretch is the image (width) 
+            this.size,                   // How stretch is the image (height)
+            (this.worldX * this.size),   // The x coordinate where to place the image on the canvas
+            (this.worldY * this.size),   // The y coordinate where to place the image on the canvas
+            this.tilesetWidth,           // The width of the image to use (stretch or crop)
+            this.tilesetHeight           // The height of the image to use (stretch or crop)
+        );         
     }
-    
+
+
 }
--- Engine/World.js
+++ Engine/World.js
@@ -43,11 +43,53 @@ export default class World{
     draw(ctx, progress){
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-        if (this.tiles.length > 0){
-            this.tiles.forEach(tile => {
-                tile.draw(ctx, progress);
-            });
-        }
+        // if (this.tiles.length > 0){
+        //     this.tiles.forEach(tile => {
+        //         tile.draw(ctx, progress);
+        //     });
+        // }
+
+        var tileSize = 32;   
+        var rowTileCount = 20;  
+        var colTileCount = 32;   
+        var imageNumTiles = 16;  
+
+        var ground = 
+        [
+            [172, 172, 172, 79, 34, 34, 34, 34, 34, 34, 34, 34, 56, 57, 54, 55, 56, 147, 67, 67, 68, 79, 79, 171, 172, 172, 173, 79, 79, 55, 55, 55],
+            [172, 172, 172, 79, 34, 34, 34, 34, 34, 34, 146, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 155, 142, 172, 159, 189, 79, 79, 55, 55, 55],
+            [172, 172, 172, 79, 79, 34, 34, 34, 34, 34, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 171, 172, 159, 189, 79, 79, 79, 55, 55, 55],
+            [188, 188, 188, 79, 79, 79, 79, 34, 34, 34, 36, 172, 172, 143, 142, 157, 79, 79, 79, 79, 79, 79, 187, 159, 189, 79, 79, 79, 55, 55, 55, 55],
+            [79, 79, 79, 79, 79, 79, 79, 79, 34, 34, 36, 172, 159, 158, 172, 143, 157, 79, 79, 79, 79, 79, 79, 79, 79, 79, 39, 51, 51, 51, 55, 55],
+            [79, 79, 79, 79, 79, 79, 79, 79, 79, 34, 36, 172, 143, 142, 172, 172, 143, 157, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 55],
+            [79, 79, 79, 79, 79, 79, 79, 79, 79, 34, 52, 172, 172, 172, 172, 172, 172, 143, 156, 157, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79],
+            [79, 79, 79, 79, 79, 79, 79, 79, 79, 34, 52, 172, 172, 172, 172, 172, 172, 159, 188, 189, 79, 79, 79, 79, 79, 171, 172, 172, 173, 79, 79, 79],
+            [79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 188, 158, 172, 172, 172, 172, 173, 79, 79, 79, 79, 79, 79, 79, 187, 158, 159, 189, 79, 79, 79],
+            [79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 171, 172, 172, 159, 188, 189, 79, 79, 79, 79, 79, 79, 79, 79, 171, 173, 79, 79, 79, 79],
+            [79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 171, 172, 172, 173, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 171, 173, 79, 79, 79, 79],
+            [155, 142, 157, 79, 79, 79, 79, 79, 79, 79, 79, 79, 187, 188, 188, 189, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 171, 173, 79, 79, 79, 79],
+            [171, 172, 173, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 171, 173, 79, 79, 79, 79],
+            [171, 172, 143, 156, 157, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 187, 189, 79, 79, 79, 79],
+            [187, 188, 158, 172, 173, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79],
+            [79, 79, 79, 188, 189, 79, 79, 79, 79, 79, 79, 155, 156, 156, 157, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 155, 156],
+            [34, 34, 79, 79, 79, 79, 79, 79, 79, 79, 79, 171, 172, 172, 173, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 155, 142, 172],
+            [34, 34, 34, 79, 79, 79, 79, 79, 79, 79, 79, 171, 172, 172, 173, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 171, 172, 172],
+            [34, 34, 34, 34, 79, 79, 79, 79, 79, 79, 155, 172, 172, 159, 189, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 171, 172, 172],
+            [34, 34, 34, 34, 34, 34, 79, 79, 79, 79, 171, 172, 172, 173, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 155, 142, 172, 172]
+        ];
+
+        const tilesetImage = new Image();
+        tilesetImage.src = "../TEST/tileset.png";
+
+        for (var r = 0; r < rowTileCount; r++) {
+            for (var c = 0; c < colTileCount; c++) {
+               var tile = ground[ r ][ c ];
+               var tileRow = (tile / imageNumTiles) | 0;
+               var tileCol = (tile % imageNumTiles) | 0;
+               ctx.drawImage(tilesetImage, (tileCol * tileSize), (tileRow * tileSize), tileSize, tileSize, (c * tileSize), (r * tileSize), tileSize, tileSize);
+            }
+         }
+
 
         if (this.gameObjects.length > 0){
             this.gameObjects.forEach(gameObject => {
@@ -56,17 +98,29 @@ export default class World{
         }
     }
 
-    // Takes the tileset and creates the tiles from it and adds them to the world
     loadTileMap(tileset, tileSize, tileMap){
-        for (let y = 0; y < tileMap.length; y++){
-            for (let x = 0; x < tileMap[y].length; x++){
-                const tile = tileMap[y][x];
-                if (tile !== 0){
-                    this.tiles.push(new Tile(x * tileSize, y * tileSize, tileset, tileSize));
-                }
-            }
-        }
-        console.log(this.tiles);
+        // const tilesPerRow = tileMap[0].length;
+        // for (let y = 0; y < tileMap.length; y++){
+        //     for (let x = 0; x < tileMap[y].length; x++){
+        //         const tile = tileMap[y][x];
+        //         const tileRow = (tile / tilesPerRow) | 0;
+        //         const tileCol = (tile % tilesPerRow) | 0;
+        //         if (tile !== 0){
+        //             this.tiles.push(
+        //                 new Tile(
+        //                     x,   // X Cord of the tile 
+        //                     y,   // Y Cord of the tile
+        //                     tileset,        // Tileset (image full of tiles)
+        //                     tileSize,       // Tile size
+        //                     tileRow,
+        //                     tileCol,
+        //                     x,              // X Cord of the world
+        //                     y,              // Y Cord of the world
+        //                     ));
+        //         }
+        //     }
+        // }
+        // console.log(this.tiles);
     }
 
 }
